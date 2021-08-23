d27c2d3e457a7662ebec53dd829f56131829d2ba
tales <talesworks@gmail.com>
2021-08-23 09:29:41 +0800

    add - dependencies

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+//css
+import '@/scss/water-light.css';
+import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
+import '@/scss/base.scss';
+
 createApp(App).use(store).use(router).mount('#app');

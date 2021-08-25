0b3044bf2d13db0256e5f8742322d9e82aeaef0e
tales <talesworks@gmail.com>
2021-08-26 06:28:03 +0800

    add - PWA dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './registerServiceWorker';
 import router from './router';
-import store from './store';
+// import store from './store';
 
 //css
 import '@/scss/water-light.css';
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
 import '@/scss/base.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  // .use(store)
+  .use(router)
+  .mount('#app');
